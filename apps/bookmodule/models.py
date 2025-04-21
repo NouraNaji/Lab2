462d0f333c996898362f3d7114b1caae8f37536c
NouraNaji <noarhnaji@gmail.com>
2025-04-21 18:46:23 +0300

    Initial commit

--- apps/bookmodule/models.py
+++ apps/bookmodule/models.py
@@ -1,7 +1,67 @@
 from django.db import models
-
+#lab8
 class Book(models.Model):
     title = models.CharField(max_length = 50)
     author = models.CharField(max_length = 50)
     price = models.FloatField(default = 0.0)
     edition = models.SmallIntegerField(default = 1)
+
+class Address(models.Model):
+    city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.city
+
+# class Student(models.Model):
+#     name = models.CharField(max_length=255)
+#     age = models.IntegerField()
+#     address = models.ForeignKey(Address, on_delete=models.CASCADE)  # ForeignKey إلى Address
+
+#     def __str__(self):
+#         return self.name
+    
+#lab9
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    price = models.FloatField(default=0.0)
+    edition = models.SmallIntegerField(default=1)
+
+class Address(models.Model):
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.city
+
+class Card(models.Model):
+    card_number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return str(self.card_number)
+    
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    code = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} ({self.code})"
+    
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    card = models.OneToOneField(Card, on_delete=models.PROTECT, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
+    course = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
+    

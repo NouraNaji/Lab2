dfa7227034376829cfbdaf746949d038e3492b57
NouraNaji <noarhnaji@gmail.com>
2025-05-06 23:54:07 +0300

    Initial commit

--- new_libraryproject/urls.py
+++ new_libraryproject/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from apps.bookmodule import views  
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("books/", include("apps.bookmodule.urls")),  # تأكد أن include يشير إلى الملف الصحيح
+    path('', views.index, name='index'),  
+    path('books/', include("apps.bookmodule.urls")),  
+    path('users/', include("apps.usermodule.urls")),  
+    
+    path('users/', include('users.urls')),  
+
 ]
 
 if settings.DEBUG:
